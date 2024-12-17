186120d17d7d3c7b208a942e9de20c7acfd099ff
Kieran Sweeden <kieransweeden@gmail.com>
2024-12-17 20:38:19 +0000

    add initial window with 2d camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
